806e0013c97b40d2eef97cf3fb240fbdb89b5999
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-03-25 22:03:04 +0900

    jmcmod 에 테스트 목적으로 UmpOcMain 클래스 추가

---
+++ src/main/java/kr/pe/ecmaxp/umpoc/console/UmpOcMain.java
@@ -0,0 +1,15 @@
+package kr.pe.ecmaxp.umpoc.console;
+
+import unicorn.Unicorn;
+
+import static unicorn.UnicornConst.*;
+import static unicorn.ArmConst.*;
+
+public class UmpOcMain
+{
+    public static void main(String[] args)
+    {
+        Unicorn uni = new Unicorn(UC_ARCH_ARM, UC_MODE_THUMB);
+
+    }
+}
