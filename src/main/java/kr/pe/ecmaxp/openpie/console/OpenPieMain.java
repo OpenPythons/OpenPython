c6e638b4cbcd8d4c0c51f0934860a9330485d117
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-08-10 00:46:22 +0900

    새로운 junicorn 테스트

--- src/main/java/kr/pe/ecmaxp/openpie/console/OpenPieMain.java
+++ src/main/java/kr/pe/ecmaxp/openpie/console/OpenPieMain.java
@@ -1,97 +1,114 @@
 package kr.pe.ecmaxp.openpie.console;
 
-import unicorn.*;
+import junicorn.MemoryAccessHook;
+import junicorn.Unicorn;
+import junicorn.UnicornException;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.file.Files;
 
-import static unicorn.ArmConst.UC_ARM_REG_PC;
-import static unicorn.UnicornConst.*;
+import static junicorn.UnicornNative.uc_version;
+import static junicorn.ArmConst.UC_ARM_REG_PC;
+import static junicorn.UnicornConst.UC_PROT_EXEC;
+import static junicorn.UnicornConst.UC_PROT_READ;
+import static junicorn.UnicornConst.UC_PROT_WRITE;
+import static junicorn.UnicornConst.*;
 
 public class OpenPieMain
 {
-    private static Unicorn uc;
-    private static int pos = 0;
+    static int pos = 0;
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnicornException, IOException, InterruptedException
     {
-        try
-        {
-            System.loadLibrary("unicorn_x64");
-            uc = new Unicorn(UC_ARCH_ARM, UC_MODE_THUMB);
+        System.loadLibrary("unicorn_x64");
+        Unicorn uc = new Unicorn(UC_ARCH_ARM, UC_MODE_THUMB);
 
-            File file = new File("C:\\Users\\EcmaXp\\Dropbox\\Projects\\openpie\\oprom\\build\\firmware.bin");
-            byte[] firmware = Files.readAllBytes(file.toPath());
-            uc.mem_map(0x08000000, 0x100000, UC_PROT_READ | UC_PROT_EXEC); // flash
-            uc.mem_map(0x20000000, 0x80000, UC_PROT_READ | UC_PROT_WRITE); // sram
-            uc.mem_map(0x3FFF0000, 0x10000, UC_PROT_READ | UC_PROT_WRITE); // stack
-            uc.mem_map(0x40000000, 0x10000, UC_PROT_READ | UC_PROT_WRITE); // peripheral
-            uc.mem_map(0xE0100000, 0x10000, UC_PROT_READ | UC_PROT_WRITE); // syscall
+        File file = new File("C:\\Users\\EcmaXp\\Dropbox\\Projects\\openpie\\oprom\\build\\firmware.bin");
+        byte[] firmware = Files.readAllBytes(file.toPath());
+        uc.mem_map(0x08000000, 0x100000, UC_PROT_READ | UC_PROT_EXEC); // flash
+        uc.mem_map(0x20000000, 0x80000, UC_PROT_READ | UC_PROT_WRITE); // sram
+        uc.mem_map(0x3FFF0000, 0x10000, UC_PROT_READ | UC_PROT_WRITE); // stack
+        uc.mem_map(0x40000000, 0x10000, UC_PROT_READ | UC_PROT_WRITE); // peripheral
+        uc.mem_map(0xE0100000, 0x10000, UC_PROT_READ | UC_PROT_WRITE); // syscall
 
-            long flash = 0x08000000;
-            uc.mem_write(flash, firmware);
-            uc.reg_write(UC_ARM_REG_PC, 0x8019614);
+        long flash = 0x08000000;
+        uc.mem_write(flash, firmware);
+        uc.reg_write(UC_ARM_REG_PC, 0x8019614);
 
-            byte[] line = "print(1, 2, 3)\r\n".getBytes();
+        byte[] line = "print(1, 2, 3)\r\n".getBytes();
 
-            uc.hook_add((ReadHook) (u, address, size, user) ->
-            {
-                // if invaild page then error
-                // uc.mem_write(address, new byte[]{1, 2, 3, 4});
+        uc_version();
 
-                int value = 0;
-                if (address == 0x4000010c) {
-                    value = 0x80000;
-                } else if (address == 0x40000110) {
-                    value = 0x10000;
-                } else if (address == 0x40000004) {
-                    if (pos < line.length) {
-                        byte ch = line[pos];
-                        value = ch;
-                        pos++;
+        uc.hook_add(
+                UC_HOOK_MEM_READ,
+                (MemoryAccessHook)(uc2, type, address, size, value, user) ->
+                {
+                    try{
+                        // if invaild page then error
+                        // uc.mem_write(address, new byte[]{1, 2, 3, 4});
+                        if (address == 0x4000010c)
+                        {
+                            value = 0x80000;
+                        }
+                        else if (address == 0x40000110)
+                        {
+                            value = 0x10000;
+                        }
+                        else if (address == 0x40000004)
+                        {
+                            if (pos < line.length)
+                            {
+                                byte ch = line[pos];
+                                value = ch;
+                                pos++;
+                            }
+                        }
+                        else
+                        {
+                            System.out.printf("read: %x, %d\r\n", address, size);
+                        }
+
+                        ByteBuffer buffer = ByteBuffer.allocate(4);
+                        buffer.order(ByteOrder.LITTLE_ENDIAN);
+                        buffer.putInt((int) value);
+
+                        byte[] buf = buffer.array();
+                        uc2.mem_write(address, buf);
+                    } catch (UnicornException ignored)
+                    {
                     }
-                } else {
-                    System.out.printf("read: %x, %d\r\n", address, size);
-                }
+                },
+                0x40000000,
+                0x40000000 + 0x10000,
+                null
+        );
 
-                ByteBuffer buffer = ByteBuffer.allocate(4);
-                buffer.order(ByteOrder.LITTLE_ENDIAN);
-                buffer.putInt(value);
+        uc.hook_add(
+                UC_HOOK_MEM_WRITE,
+                (MemoryAccessHook) (uc2, type, address, size, value, user) ->
+                {
+                    if (address == 0x40000000)
+                    {
+                        System.out.append((char) value);
+                    }
+                    // System.out.printf("write: %x, %d, %d\n", address, size, value);
+                },
+                0x40000000,
+                0x40000000 + 0x10000,
+                null
+        );
 
-                byte[] buf = buffer.array();
-                u.mem_write(address, buf);
-            }, 0x40000000, 0x40000000 + 0x10000, null);
-
-            uc.hook_add((uc, address, size, value, user) ->
-            {
-                if (address == 0x40000000) {
-                    System.out.append((char)value);
-                }
-                // System.out.printf("write: %x, %d, %d\n", address, size, value);
-            }, 0x40000000, 0x40000000 + 0x10000, null);
-
-            uc.hook_add((u, address, size, value, user) ->
-            {
-                System.out.printf("failure: %x, %d, %d\n", address, size, value);
-                return false;
-            }, UC_HOOK_MEM_READ_UNMAPPED, null);
-
-            while (true) {
-                long addr = ((Number)uc.reg_read(UC_ARM_REG_PC)).longValue();
-                uc.emu_start(addr | 1, flash + 0x100000, 0, 0);
-            }
-        }
-        catch (Exception e)
+        //noinspection InfiniteLoopStatement
+        do
         {
-            System.out.println(uc.reg_read(UC_ARM_REG_PC));
-            e.printStackTrace();
+            long addr = uc.reg_read(UC_ARM_REG_PC);
+            uc.emu_start(addr | 1, flash + 0x100000, 0, 10000);
         }
-    }
+        while (true);
 
-    private static int fromByteArray(byte[] bytes)
-    {
-        return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+        // uc.close();
     }
 }
