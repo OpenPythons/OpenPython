4506393f9e2f4c0ab3438a0b2f57b699d3bda336
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-09-01 21:10:33 +0900

    Change _SVC_CALL to __syscall

--- src/main/java/kr/pe/ecmaxp/openpie/Interrupt.java
+++ src/main/java/kr/pe/ecmaxp/openpie/Interrupt.java
@@ -11,13 +11,16 @@ public class Interrupt
     public final int r1;
     public final int r2;
     public final int r3;
+    public final int r4;
 
     public Interrupt(CPU cpu, int imm)
     {
-        this.imm = imm;
+        assert imm == 0;
+        this.imm = cpu.regs.get(R7);
         this.r0 = cpu.regs.get(R0);
         this.r1 = cpu.regs.get(R1);
         this.r2 = cpu.regs.get(R2);
         this.r3 = cpu.regs.get(R3);
+        this.r4 = cpu.regs.get(R4);
     }
 }
