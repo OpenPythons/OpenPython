2e876232c33f57b17e6ff19515dead4340cd4997
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-09-19 01:05:05 +0900

    Remove example micropython (AOT)

--- src/main/java/kr/pe/ecmaxp/openpie/console/OpenMachineMain.kt
+++ src/main/java/kr/pe/ecmaxp/openpie/console/OpenMachineMain.kt
@@ -1,7 +1,5 @@
 package kr.pe.ecmaxp.openpie.console
 
-import kr.pe.ecmaxp.micropython.example.MicroPython
-import kr.pe.ecmaxp.micropython.example.MicroPython_link.Reset_Handler
 import kr.pe.ecmaxp.thumbjk.Interrupt
 import kr.pe.ecmaxp.thumbsk.CPU
 import kr.pe.ecmaxp.thumbsk.Memory
@@ -24,8 +22,7 @@ object OpenMachineMain {
     @Throws(Exception::class, ControlPauseSignal::class, ControlStopSignal::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        val mp = MicroPython(Memory())
-        val memory = mp.memory
+        val memory = Memory()
 
         val file = File("C:\\Users\\EcmaXp\\Dropbox\\Projects\\openpie\\oprom\\build\\firmware.bin")
         val firmware = Files.readAllBytes(file.toPath())
@@ -64,24 +61,10 @@ object OpenMachineMain {
         memory.map(0xE0000000L, 16 * KB, MemoryFlag.RW) // syscall
         memory.writeBuffer(0x08000000, firmware)
 
-        mp.refCPU.regs[RegisterIndex.PC] = Reset_Handler
-        assert(memory.readInt(0x08000000 + 4) == Reset_Handler)
-
-        /*
-        private fun interruptResponseBuffer(buffer: ByteArray) {
-            cpu!!.memory.writeInt(bufAddress, bufAddress + 8) // + 0
-            cpu!!.memory.writeInt(bufAddress + 4, buffer.size) // + 4
-            cpu!!.memory.writeBuffer(bufAddress + 8, buffer) // + 8
-            cpu!!.memory.writeByte(bufAddress + 8 + buffer.size, 0.toByte()) // + 8 + n
-            cpu!!.regs.set(R0, bufAddress)
-        }
-         */
-
-
         val refMemory = memory.copy()
         var refCPU = CPU()
         refCPU.memory = refMemory
-        refCPU.regs[RegisterIndex.PC] = Reset_Handler
+        refCPU.regs[RegisterIndex.PC] = memory.readInt(0x08000000 + 4)
         refCPU.interruptHandler = {
             throw Exception(it.toString())
         }
@@ -98,7 +81,7 @@ object OpenMachineMain {
 
         refCPU = CPU()
         refCPU.memory = refMemory
-        refCPU.regs[RegisterIndex.PC] = Reset_Handler
+        refCPU.regs[RegisterIndex.PC] = memory.readInt(0x08000000 + 4)
         refCPU.interruptHandler = {
             throw Exception(it.toString())
         }
@@ -112,31 +95,5 @@ object OpenMachineMain {
             println("done $e")
             println(Instant.now().toEpochMilli() - start3.toEpochMilli())
         }
-
-        val newMemory = memory.copy()
-        val newMp = MicroPython(newMemory)
-        newMp.refCPU.regs[RegisterIndex.PC] = Reset_Handler
-
-        try {
-            mp.Reset_Handler {
-                svc ->
-                run {
-                    println(svc)
-                    throw Exception()
-                }
-            }
-        } catch (e: Exception){
-        }
-
-        val start = Instant.now()
-        newMp.Reset_Handler {
-            svc ->
-            run {
-                println(svc)
-                println(Instant.now().toEpochMilli() - start.toEpochMilli())
-                exitProcess(0);
-            }
-        }
-
     }
 }
