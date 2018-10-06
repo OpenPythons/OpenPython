212736ddeecb5ccc2a4ece99f27d4958a2b49665
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-10-06 14:47:49 +0900

    optvm (Java side)

---
+++ src/main/java/kr/pe/ecmaxp/optvm/ECPU.kt
@@ -0,0 +1,12 @@
+package kr.pe.ecmaxp.optvm
+
+class ECPU {
+    init {
+        System.loadLibrary("optvm")
+        init()
+    };
+
+    external fun init()
+    external fun step(): IntArray
+    external fun debug(address: Int)
+}
