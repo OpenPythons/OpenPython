f630dd4ad2c4e1adeaa1bd0b4e17d23805b7e4b7
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-10-07 23:06:26 +0900

    VMState 에서 필요없는 변수 삭제

--- src/main/java/kr/pe/ecmaxp/openpie/arch/state/VMState.kt
+++ src/main/java/kr/pe/ecmaxp/openpie/arch/state/VMState.kt
@@ -1,15 +1,9 @@
 package kr.pe.ecmaxp.openpie.arch.state
 
-import kr.pe.ecmaxp.openpie.arch.types.Interrupt
-import li.cil.oc.api.machine.Signal
 import java.util.*
 
 class VMState {
-    var redirectKeyEvent = true
-    var lastInterrupt: Interrupt? = null
+    // TODO: component and method?
     var fdCount = 3
     var fdMap: HashMap<Int, FileHandle> = HashMap()
-    val signals: LinkedList<Signal> = LinkedList()
-    val inputBuffer: LinkedList<Char> = LinkedList()
-    var outputBuffer: StringBuilder = StringBuilder()
 }
