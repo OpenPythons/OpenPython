c4dc1a978d2c873aa9850abbfbce85b23966c4b0
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-10-07 18:05:51 +0900

    파일 경로 변경

--- src/main/java/kr/pe/ecmaxp/openpie/OpenPieFilePaths.kt
+++ src/main/java/kr/pe/ecmaxp/openpie/OpenPieFilePaths.kt
@@ -1,6 +1,6 @@
 package kr.pe.ecmaxp.openpie
 
 object OpenPieFilePaths {
-    val MapFile = "C:\\Users\\EcmaXp\\Dropbox\\Projects\\openpie\\oprom\\build\\firmware.elf.map"
-    val FirmwareFile = "C:\\Users\\EcmaXp\\Dropbox\\Projects\\openpie\\oprom\\build\\firmware.bin"
+    val MapFile = "C:\\Users\\EcmaXp\\Dropbox\\Projects\\OpenPie\\opmod\\src\\main\\resources\\assets\\openpie\\firmwares\\debug\\firmware.map"
+    val FirmwareFile = "C:\\Users\\EcmaXp\\Dropbox\\Projects\\OpenPie\\opmod\\src\\main\\resources\\assets\\openpie\\firmwares\\debug\\firmware.bin"
 }
