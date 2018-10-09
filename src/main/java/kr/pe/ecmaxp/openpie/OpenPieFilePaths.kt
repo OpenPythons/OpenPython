67cf89ad5055379e8b1d7bf0d11399077a436891
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-10-09 22:28:33 +0900

    OpenPieFilePaths 제거

--- src/main/java/kr/pe/ecmaxp/openpie/OpenPieFilePaths.kt
+++
@@ -1,6 +0,0 @@
-package kr.pe.ecmaxp.openpie
-
-object OpenPieFilePaths {
-    val MapFile = "C:\\Users\\EcmaXp\\Dropbox\\Projects\\OpenPie\\opmod\\src\\main\\resources\\assets\\openpie\\firmwares\\debug\\firmware.map"
-    val FirmwareFile = "C:\\Users\\EcmaXp\\Dropbox\\Projects\\OpenPie\\opmod\\src\\main\\resources\\assets\\openpie\\firmwares\\debug\\firmware.bin"
-}
