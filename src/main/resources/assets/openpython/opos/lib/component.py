fdbc94d04ef56a49bdf916c7178ba3acd84e8790
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-10-23 23:47:24 +0900

    opos /lib/component.py 업데이트

--- src/main/resources/assets/openpython/opos/lib/component.py
+++ src/main/resources/assets/openpython/opos/lib/component.py
@@ -1,11 +1,17 @@
-from ucomponent import invoke, get_methods, get_doc, get_list
+from ucomponent import invoke, get_methods, get_doc, get_list, get_type, get_slot
 
-__all__ = ['Component', 'get_component', 'find_components', 'components']
+import event
 
-PRIMARY_COMPONENTS = {}
+_list = list
+
+__all__ = ['Component', 'is_available', 'get_primary', 'get_primary_checked', 'set_primary']
+
+primaries = {}
 
 
 class ComponentMethod:
+    __slots__ = "component", "name"
+
     def __init__(self, component, name):
         self.component = component
         self.name = name
@@ -15,7 +21,7 @@ class ComponentMethod:
 
     @property
     def __doc__(self):
-        return get_doc(self.component.address, self.name)
+        return doc(self.component.address, self.name)
 
     def __repr__(self):
         doc = self.__doc__
@@ -28,47 +34,119 @@ class ComponentMethod:
 
 
 class Component:
-    def __init__(self, address, type):
+    __slots__ = "address",
+
+    def __init__(self, address):
         self.address = address
-        self.type = type
+
+    @property
+    def type(self):
+        return get_type(self.address)
+
+    @property
+    def slot(self):
+        return get_slot(self.address)
 
     def __getattr__(self, name):
         return ComponentMethod(self, name)
 
     def __dir__(self):
-        return dir(object()) + ["address", "type"] + list(get_methods(self.address))
+        return dir(object()) + ["address", "type", "slot"] + methods(self.address)
 
     def __repr__(self):
         return "Component<{0}:{1}>".format(self.type, self.address)
 
 
-components = get_list  # TODO: ?
+def doc(address: str, method: str):
+    return get_doc(address, method)
 
 
-def set_primary(compoent):
-    PRIMARY_COMPONENTS[compoent.type] = compoent
+# noinspection PyShadowingBuiltins
+def list(filter: str = None, exact: bool = True):
+    if filter is None:
+        return [proxy(address) for address in get_list()]
+    elif exact:
+        return [proxy(address) for address in get_list(filter)]
+    else:
+        return [proxy(address)
+                for address, component_type
+                in get_list().items()
+                if filter in component_type]
 
 
-def get_component(component_type):
-    component = PRIMARY_COMPONENTS.get(component_type)
-    if component:
-        return component
-
-    for address in get_list(component_type):
-        component = Component(address, component_type)
-        set_primary(component)
-        return component
-
-    return None
+def methods(address: str) -> _list:
+    return _list(get_methods(address))
 
 
-# alias
-get = get_component
+def proxy(address: str):
+    return Component(address)
 
 
-def find_components(component_type):
-    return [Component(address, component_type) for address in get_list(component_type)]
+def type(address: str):
+    return get_type(address)
 
 
-# alias
-find = find_components
+def slot(address: str) -> int:
+    slot = get_slot(address)
+    return slot if slot is not None else -1
+
+
+def fields(address: str):
+    raise NotImplementedError
+
+
+def get(address: str, component_type: str):
+    size = len(address)
+
+    for addr, compType in get_list(component_type):
+        if addr[:size] == address:
+            return proxy(addr)
+
+    raise Exception("no such component")
+
+
+def is_available(component_type: str):
+    return primaries.get(component_type) is not None
+
+
+def get_primary(component_type: str) -> Component:
+    return primaries.get(component_type)
+
+
+def get_primary_checked(component_type: str) -> Component:
+    if not is_available(component_type):
+        raise Exception("no primary {!r} available".format(component_type))
+
+    return primaries[component_type]
+
+
+def load_primary(component_type: str) -> Component:
+    return primaries.get(component_type)
+
+
+def set_primary(component_type: str, address: str):
+    primaries[component_type] = proxy(address)
+
+
+@event.register("component_added")
+def on_component_added(_, address, component_type):
+    prev = primaries.get(component_type)
+    if prev is None:
+        primaries[component_type] = proxy(address)
+
+
+@event.register("component_removed")
+def on_component_removed(_, address, component_type):
+    prev = primaries.get(component_type)
+    if prev is not None and prev.address == address:
+        del primaries[component_type]
+
+
+def __getattr__(name: str) -> Component:
+    return get_primary_checked(name)
+
+
+def setup():
+    for address, component_type in get_list().items():
+        if not is_available(component_type):
+            set_primary(component_type, address)
